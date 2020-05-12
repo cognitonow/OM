d3698436a611bbceb3393ea305f4b2fe08cca6a5
cognitonow <cognito.learning.now@gmail.com>
2020-05-12 14:42:58 +0200

    asd
    asd

---
+++ story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Rs1v3WPFB4":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("completed")
+}
+
